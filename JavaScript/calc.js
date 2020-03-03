96419766e6a307069989107f853deecdd205051a
eiribarev <eiribarev@codingburgas.bg>
2020-03-03 14:05:49 +0200

    Implemented displaying the calculated result

--- JavaScript/calc.js
+++ JavaScript/calc.js
@@ -9,7 +9,7 @@ function validateInput() {
 
 		document.getElementById("fullNumber").style.removeProperty("opacity");
 
-		setTimeout(function() {document.getElementById("calculator").style.removeProperty("height");}, 700);
+		setTimeout(function() {document.getElementById("calculator").style.removeProperty("height");}, 300);
 	}
 	else {
 		errorMessage.removeAttribute("style");
@@ -18,14 +18,58 @@ function validateInput() {
 
 		document.getElementById("fullNumber").style.display = "inline-block";
 
-		setTimeout(function() {handleNumber(inputText);}, 700);
+		updateNumber(inputText);
 	}
 }
-
-function handleNumber(stockNumber) {
+function updateNumber(stockNumber) {
 	let control = String(controlDigit(stockNumber));
 
-	document.getElementById("fullNumber").style.opacity = "1";
+	document.getElementById("fullNumber").style.removeProperty("opacity");
+
+	setTimeout(function() {
+		if (stockNumber.length == 5){
+			document.getElementById("stockType").style.visibility = "hidden";
+			document.getElementById("countryCode").style.visibility = "hidden";
+			document.getElementById("countryCode").style.marginRight = "10px";
+			document.getElementById("leadingZeroes").style.visibility = "hidden";
+			document.getElementById("stockClass").style.marginLeft = "-95px";
+
+			document.getElementById("stockClass").innerHTML = stockNumber.substr(0, 2);
+			document.getElementById("stockNumber").innerHTML = stockNumber.substr(2, 3);
+			document.getElementById("controlDigitSeparator").innerHTML = ".";
+			document.getElementById("controlDigit").innerHTML = control;
+		}
+		if (stockNumber.length == 9){
+			document.getElementById("stockType").style.removeProperty("visibility");
+			document.getElementById("countryCode").style.removeProperty("visibility");
+			document.getElementById("countryCode").style.marginRight = "10px";
+			document.getElementById("leadingZeroes").style.visibility = "hidden";
+			document.getElementById("stockClass").style.marginLeft = "-62px";
+
+			document.getElementById("stockType").innerHTML = stockNumber.substr(0, 2);
+			document.getElementById("countryCode").innerHTML = stockNumber.substr(2, 2);
+			document.getElementById("stockClass").innerHTML = stockNumber.substr(4, 2);
+			document.getElementById("stockNumber").innerHTML = stockNumber.substr(6, 3);
+			document.getElementById("controlDigitSeparator").innerHTML = ".";
+			document.getElementById("controlDigit").innerHTML = control;
+		}
+		if (stockNumber.length == 11){
+			document.getElementById("stockType").style.removeProperty("visibility");
+			document.getElementById("countryCode").style.removeProperty("visibility");
+			document.getElementById("countryCode").style.removeProperty("margin-right");
+			document.getElementById("leadingZeroes").style.removeProperty("visibility");
+			document.getElementById("stockClass").style.removeProperty("margin-left");
+
+			document.getElementById("stockType").innerHTML = stockNumber.substr(0, 2);
+			document.getElementById("countryCode").innerHTML = stockNumber.substr(2, 2);
+			document.getElementById("stockClass").innerHTML = stockNumber.substr(6, 2);
+			document.getElementById("stockNumber").innerHTML = stockNumber.substr(8, 3);
+			document.getElementById("controlDigitSeparator").innerHTML = "-";
+			document.getElementById("controlDigit").innerHTML = control;
+		}
+	}, 500)
+
+	setTimeout(function() {document.getElementById("fullNumber").style.opacity = "1";}, 800);
 }
 
 function controlDigit(stockNumber) {
